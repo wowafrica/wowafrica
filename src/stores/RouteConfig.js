3b9ec9dc0add36ea432f91881a29784816cc3dd0
Lee <jessy1092@gmail.com>
2015-03-01 19:57:45 +0800

    Fix view_africa typo.

--- src/stores/RouteConfig.js
+++ src/stores/RouteConfig.js
@@ -6,37 +6,37 @@ export default {
     path: '/',
     method: 'get',
     page: require('../pages/IndexPage'),
-    subPage: ['view_afica']
+    subPage: ['view_africa']
   },
-  view_afica: {
+  view_africa: {
     showName: '認識非洲',
-    path: '/view_afica',
+    path: '/view_africa',
     method: 'get',
     page: require('../pages/ViewAfricaPage'),
-    subPage: ['view_afica_nations', 'view_afica_people']
+    subPage: ['view_africa_nations', 'view_africa_people']
   },
-  view_afica_nations: {
+  view_africa_nations: {
     showName: '國家基本資料',
-    path: '/view_afica/nations',
+    path: '/view_africa/nations',
     method: 'get',
     page: require('../pages/IndexPage'),
     subPage: []
   },
-  view_afica_nations_post: {
-    path: 'view_afica/nations/posts/:post',
+  view_africa_nations_post: {
+    path: 'view_africa/nations/posts/:post',
     method: 'get',
     page: require('../pages/IndexPage'),
     subPage: []
   },
-  view_afica_people: {
+  view_africa_people: {
     showName: '非洲人物誌',
-    path: '/view_afica/people',
+    path: '/view_africa/people',
     method: 'get',
     page: require('../pages/IndexPage'),
     subPage: []
   },
-  view_afica_people_post: {
-    path: 'view_afica/people/posts/:post',
+  view_africa_people_post: {
+    path: 'view_africa/people/posts/:post',
     method: 'get',
     page: require('../pages/IndexPage'),
     subPage: []
