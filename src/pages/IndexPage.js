c4f2e384a7808574abd5a2deb1220036c0806c56
Hank Wu <wutzuhan@gmail.com>
2015-04-19 23:24:09 +0800

    Index page header image and title skeleton

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -14,6 +14,12 @@ export default React.createClass({
           <IndexMenu />
         </div>
         <div className="container-content">
+          <div className="header" style={{marginTop: '1%', minHeight: '600px', textAlign: 'center', backgroundPosition: 'top center', backgroundImage: 'url(https://fbcdn-sphotos-f-a.akamaihd.net/hphotos-ak-xpa1/t31.0-8/11154796_779010435550566_7018350735656129504_o.jpg)'}}>
+            <div className="hearder-box" style={{paddingTop: '50px'}}>
+              <p className="header-title" style={{fontSize: '100px', letterSpacing: '38px', fontWeight: 'bolder', marginBottom: '0'}}>WOW!</p>
+              <p className="header-title" style={{fontSize: '80px', letterSpacing: '34px', fontWeight: 'bolder'}}>AFRICA</p>
+            </div>
+          </div>
         </div>
       </div>
     );
