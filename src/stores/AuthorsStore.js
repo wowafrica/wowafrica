75ee7d9a948b207ec0617a2ecfed01e16942b747
Lee <jessy1092@gmail.com>
2015-08-09 22:35:55 +0800

    Add AuthorsStore.js and NationsStore-test.js test case. issue #19

--- src/stores/AuthorsStore.js
+++ src/stores/AuthorsStore.js
@@ -24,37 +24,29 @@ class AuthorsStore extends EventEmitter {
   }
 
   loadAuthorData() {
-    let {tag, picWidth} = TumblrConfig.author;
+    let {tag} = TumblrConfig.author;
+    this.client.posts(TumblrConfig.blogName, {tag, filter: 'text'}, this.parseAuthorData.bind(this));
+  }
 
-    this.client.posts(TumblrConfig.blogName, {tag: tag, filter: 'text'}, (err, data) => {
-      if (err) {
-        console.log(err.stack);
-      }
+  parseAuthorData(err, data) {
+    if (err) {
+      console.log(err.stack);
+    }
+    let {picWidth} = TumblrConfig.author;
 
-      let tmpAuthors = [];
-      data.posts.map((post) => {
-        let contents = post.caption.split('\n\n');
-        let targetUrl = '';
-        for ( let i = 0; i < post.photos[0].alt_sizes.length; i++ ) {
-          if ( post.photos[0].alt_sizes[i].width < picWidth ) {
-            targetUrl = post.photos[0].alt_sizes[i].url;
-            break;
-          }
-        }
+    this.authors = data.posts.map((post) => {
+      let contents = post.caption.split('\n\n');
+      let target = post.photos[0].alt_sizes.filter(photo => photo.width < picWidth);
 
-        let newAuthor = {
-          id: post.id,
-          name: contents[0],
-          from: contents[1],
-          description: contents[2],
-          photoUrl: targetUrl
-        };
-
-        tmpAuthors.push(newAuthor);
-      });
-      this.authors = tmpAuthors;
-      this.emitChange();
+      return {
+        id: post.id,
+        name: contents[0],
+        from: contents[1],
+        description: contents[2],
+        photoUrl: target.length ? target[0].url : ''
+      };
     });
+    this.emitChange();
   }
 
   emitChange() {
