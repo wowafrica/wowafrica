ef6059a37ce7f21070ba374ad2af5ec89c356e11
Lee <jessy1092@gmail.com>
2015-09-14 20:41:46 +0800

    Update: Modify tumblr domain.

--- src/configures/TumblrConfig.js
+++ src/configures/TumblrConfig.js
@@ -1,6 +1,6 @@
 export default {
 
-  blogName: 'exploreafrica-tw.tumblr.com',
+  blogName: 'wowafrica.tumblr.com',
   consumerKey: 'M5o8MnMmq8jAwmmj82HhkyPNkiI6mq9VccTZzYZZLZPgfl08Hi',
 
   author: {
