d0d6363db9b707fe619a1aa5e8ed0a8d83e14846
Hank Wu <wutzuhan@gmail.com>
2015-10-04 22:39:50 +0800

    Add center logo in category boxes

--- src/components/IndexCategoryBlock.js
+++ src/components/IndexCategoryBlock.js
@@ -8,35 +8,51 @@ let {Cards, Card, Image} = Semantify;
 let CategoryBox = React.createClass({
 
   getInitialState() {
-    return {
-      posts: PostListStore.getPostList(this.props.category)
-    };
+    if (this.props.category == 'none') {
+      return {
+        posts: []
+      };
+    }
+    else {
+      return {
+        posts: PostListStore.getPostList(this.props.category)
+      };
+    }
   },
 
   render() {
-    let backImg = '';
-    let title = '';
-    let url = '';
-    if (this.state.posts.length > 0) {
-      backImg = this.state.posts[0].image;
-      title = this.state.posts[0].title;
-      url = '/view_post_list/posts/'+this.state.posts[0].id;
+    if (this.props.category == 'none') {
+      return (
+        <div className="middle aligned centered column">
+          <img src="/images/icon_55.png" style={{marginLeft: '25%', width: '50%'}}/>
+        </div>
+      )
     }
-    return (
-      <div className="middle aligned column" style={{backgroundSize: 'cover', backgroundImage: 'url('+backImg+')'}}>
-          <a href={url} style={{color: 'grey'}}>
-            <div style={{width: '100%', position: 'relative'}}>
-              <div style={{margin: '20% 20% 20% 20%', paddingTop: '2%', paddingBottom: '2%', backgroundColor: 'white'}}>
-                <div style={{height: '7em',textAlign: 'center', wordWrap: 'break-word', fontSize: '20px', lineHeight: '1.5em', overflow: 'hidden', margin: '1rem'}}>
-                  {PostListConfig.categoryMap[this.props.category]}
-                  <div className="ui divider" style={{borderTop: '2px solid grey', margin: '0.5rem 0rem 0.5rem 0rem'}}/>
-                  {title}
+    else {
+      let backImg = '';
+      let title = '';
+      let url = '';
+      if (this.state.posts.length > 0) {
+        backImg = this.state.posts[0].image;
+        title = this.state.posts[0].title;
+        url = '/view_post_list/posts/'+this.state.posts[0].id;
+      }
+      return (
+        <div className="middle aligned column" style={{backgroundSize: 'cover', backgroundImage: 'url('+backImg+')'}}>
+            <a href={url} style={{color: 'grey'}}>
+              <div style={{width: '100%', position: 'relative'}}>
+                <div style={{margin: '20% 20% 20% 20%', paddingTop: '2%', paddingBottom: '2%', backgroundColor: 'white'}}>
+                  <div style={{height: '7em',textAlign: 'center', wordWrap: 'break-word', fontSize: '20px', lineHeight: '1.5em', overflow: 'hidden', margin: '1rem'}}>
+                    {PostListConfig.categoryMap[this.props.category]}
+                    <div className="ui divider" style={{borderTop: '2px solid grey', margin: '0.5rem 0rem 0.5rem 0rem'}}/>
+                    {title}
+                  </div>
                 </div>
               </div>
-            </div>
-          </a>
-      </div>
-    );
+            </a>
+        </div>
+      );
+    }
   },
 
   componentDidMount() {
@@ -48,9 +64,13 @@ let CategoryBox = React.createClass({
   },
 
   _onChange() {
-    this.setState({
-      posts: PostListStore.getPostList(this.props.category)
-    });
+    if (this.props.category == 'none') {
+    }
+    else {
+      this.setState({
+        posts: PostListStore.getPostList(this.props.category)
+      });
+    }
   }
 });
 
@@ -62,7 +82,12 @@ export default React.createClass({
   },
 
   render() {
-    let viewBlks = PostListConfig.categories.map((item) => {
+    let items = [];
+    PostListConfig.categories.forEach((item) => {
+      items.push(item);
+    })
+    items.splice(4, 0, 'none');
+    let viewBlks = items.map((item) => {
       return (
       <CategoryBox category={item} />
       );
