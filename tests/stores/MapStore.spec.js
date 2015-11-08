67e62d9f61cb20dc72e3632cfe605d55b31fffb2
Lee <jessy1092@gmail.com>
2015-11-08 23:29:23 +0800

    test(map): Fix test

--- tests/stores/MapStore.spec.js
+++ tests/stores/MapStore.spec.js
@@ -2,14 +2,30 @@ let fakeMap = {
   'features':[
     {'properties': {'ISO_A3': 'SOM', 'NAME':'Somalia'}},
     {'properties': {'ISO_A3': 'CAF', 'NAME':'Central African Rep.'}}
-  ]
+  ],
+  area: {
+    CentralAfrica: {},
+    EastAfrica: {},
+    NorthAfrica: {},
+    SouthernAfrica: {},
+    WestAfrica: {}
+  }
 };
 
 describe('MapStore', () => {
   it('should have initialize format', () => {
     let mapStore = require('../../src/stores/MapStore');
 
-    expect(mapStore.geoJson).toEqual({features: []});
+    expect(mapStore.geoJson).toEqual({
+      features: [],
+      area: {
+        CentralAfrica: {},
+        EastAfrica: {},
+        NorthAfrica: {},
+        SouthernAfrica: {},
+        WestAfrica: {}
+      }
+    });
   });
 
   it('should get Nation map infor by ISO', () => {
