c4856fcd46ca25cb0211d6767997e2f7f19ce6cf
Lee <jessy1092@gmail.com>
2015-12-20 22:36:21 +0800

    feat(about): Add Story and partner information

---
+++ src/configures/MemberConfig.js
@@ -0,0 +1,33 @@
+export default [
+  {
+    'name': '盧韋辰 ／ Weichen Lu',
+    'title': '共同創辦人 & 執行長 ／ Cofunder & CEO'
+  }, {
+    'name': '賴奕諭 ／ Larry Lai',
+    'title': '共同創辦人 & 總編輯 ／ Cofunder & Chief Editor'
+  }, {
+    'name': '何佩佳 ／ Pejia Ho',
+    'title': '共同創辦人 & 市場開發經理 ／ Cofunder & BD Manager'
+  }, {
+    'name': '葉菀苓 ／ Bear Ya',
+    'title': '社群總編 ／ Social Media Editor'
+  }, {
+    'name': '程可安 ／ Anna',
+    'title': '專案經理 ／ Project Director'
+  }, {
+    'name': '謝睿哲 ／ Solid',
+    'title': '專案經理 ／ Project Director'
+  }, {
+    'name': '林詩閔 ／',
+    'title': '活動總監 ／ Offline Event Director'
+  }, {
+    'name': '李俊緯 ／ Lee',
+    'title': '進擊的攻城屍 ／ Web Enginer No.1'
+  }, {
+    'name': '吳咨翰 ／ TzuHan',
+    'title': '進擊的攻城屍 ／ Web Enginer No.1'
+  }, {
+    'name': '洪暄晴 ／',
+    'title': '插花的網頁介面設計師 ／ Web UI Designer'
+  }
+];
