cab0f3b0da5d28d68ddee9760c3457fbdc42d925
Lee <jessy1092@gmail.com>
2016-06-18 22:42:49 +0800

    fix(sidebar): Modify sidebar render method
    - Sidebar need render on the root element
    - Content should be in the element which has 'pusher' class
    - Fix the RouteAction

--- server/prod/html.template.js
+++ server/prod/html.template.js
@@ -14,8 +14,8 @@ export default ({title, description, image, html}) => (
   <link href="/styles/main.css" type="text/css" rel="stylesheet"></link>
 </head>
 <body>
-  <div id="side_bar"></div>
-  <div id="content">${html}</div>
+  <div id="side_bar" class="ui inverted vertical menu sidebar"></div>
+  <div id="content" class="pusher">${html}</div>
   <div id="nation_modal" class="ui dimmer modals page"></div>
   <script type="text/javascript" src="/scripts/vendor.bundle.js"></script>
   <script type="text/javascript" src="/scripts/bundle.js"></script>
