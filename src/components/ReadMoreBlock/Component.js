c54d255f2681fc49d298a77f0cede27b700f5438
Lee <jessy1092@gmail.com>
2016-06-24 01:37:41 +0800

    fix(ReadMoreBlock): Use css modules to override the first item's style.

--- src/components/ReadMoreBlock/Component.js
+++ src/components/ReadMoreBlock/Component.js
@@ -1,32 +1,26 @@
-import React              from 'react';
+import React from 'react';
 
-let Block = ({post, idx, onBlockClick}) => {
+import styles from './index.css';
 
-  let style = {};
+let Block = ({post, idx, onBlockClick}) => (
+  <a className={`item ${styles.block}`}
+     href={`/view_post_list/posts/${post.id}`}
+     onClick={onBlockClick}>
 
-  if (idx == 0) {
-    style = {
-      borderTop: '1px solid rgba(34, 36, 38, 0.15)',
-      padding: '1em 0em !important'
-    };
-  }
+    <div className="ui medium image">
+      <img src={post.image} style={{maxWidth: '300px', maxHeight: '200px'}}/>
+    </div>
 
-  return (
-    <a className="item" href={`/view_post_list/posts/${post.id}`} onClick={onBlockClick} style={style}>
-      <div className="ui medium image">
-        <img src={post.image} style={{maxWidth: '300px', maxHeight: '200px'}}/>
+    <div className="middle aligned content">
+      <div className="ui header">
+        {post.title}
       </div>
-      <div className="middle aligned content">
-        <div className="ui header">
-          {post.title}
-        </div>
-        <div className="description">
-          {post.brief.length > 80 ? post.brief.substr(0, 80) + '...' : post.brief}
-        </div>
+      <div className="description">
+        {post.brief.length > 80 ? post.brief.substr(0, 80) + '...' : post.brief}
       </div>
-    </a>
-  )
-};
+    </div>
+  </a>
+);
 
 let ReadMoreBlock = ({posts}) => (
   <div className="ui divided link items">
