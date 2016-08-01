f3d15438e79fbc14b714dfd1cffb8900ff8feaaf
Hank Wu <wutzuhan@gmail.com>
2016-08-01 19:40:35 +0800

    fix(ActivityFilter): show current filter option based on current state

--- src/components/ActivityFilter/index.js
+++ src/components/ActivityFilter/index.js
@@ -29,22 +29,22 @@ export default React.createClass({
       <div className={styles.container}>
         <div className={styles.title}>
           <div id="activityTimeFilter" className="ui inline dropdown">
-            <div className="text">即將</div>
+            <div className="text">{activities.showOldItems ? '過去' : '即將'}</div>
             <i className="dropdown icon"></i>
             <div className="menu">
               <div className="header">活動時間</div>
-              <div className="active item" onClick={this.onShowNewClick} data-text="即將">即將</div>
-              <div className="item" onClick={this.onShowOldClick} data-text="過去">過去</div>
+              <div className={`${activities.showOldItems ? '' : 'active'} item`} onClick={this.onShowNewClick} data-text="即將">即將</div>
+              <div className={`${activities.showOldItems ? 'active' : ''} item`} onClick={this.onShowOldClick} data-text="過去">過去</div>
             </div>
           </div>
           {'由 '}
           <div id="activityHostFilter" className="ui inline dropdown">
-            <div className="text">wowAfrica</div>
+            <div className="text">{activities.showOtherHost ? '夥伴單位' : 'wowAfrica'}</div>
             <i className="dropdown icon"></i>
             <div className="menu">
               <div className="header">主辦單位</div>
-              <div className="active item" onClick={this.onShowWaClick} data-text="wowAfrica">wowAfrica</div>
-              <div className="item" onClick={this.onShowOtherClick} data-text="夥伴單位">夥伴單位</div>
+              <div className={`${activities.showOtherHost ? '' : 'active'} item`} onClick={this.onShowWaClick} data-text="wowAfrica">wowAfrica</div>
+              <div className={`${activities.showOtherHost ? 'active' : ''} item`} onClick={this.onShowOtherClick} data-text="夥伴單位">夥伴單位</div>
             </div>
           </div>
           舉辦
